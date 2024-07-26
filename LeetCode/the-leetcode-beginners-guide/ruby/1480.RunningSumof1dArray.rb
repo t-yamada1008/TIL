fda15359286d6079a266177c2aa928063a64b346
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2024-07-27 01:19:42 +0900

    Fix: 解説系を修正

--- LeetCode/the-leetcode-beginners-guide/ruby/1480.RunningSumof1dArray.rb
+++ LeetCode/the-leetcode-beginners-guide/ruby/1480.RunningSumof1dArray.rb
@@ -1,21 +1,6 @@
 # 1480. Running Sum of 1d Array
 # Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
-#
 # Return the running sum of nums.
-
-# @param {Integer[]} nums
-# @return {Integer[]}
-
-def running_sum(nums)
-  sum = 0
-  result = []
-  nums.each do|num|
-      sum = sum + num
-      result << sum
-  end
-
-  result
-end
 
 # Example 1:
 #
@@ -38,13 +23,15 @@ end
 # 1 <= nums.length <= 1000
 # -10^6 <= nums[i] <= 10^6
 
-# 別回答
+# Code
+
+# @param {Integer[]} nums
+# @return {Integer[]}
 def running_sum(nums)
-  (1...nums.length).each do |i|
-    nums[i] += nums[i - 1]
-  end
-  nums
+  sum = 0
+  nums.map { |num| sum += num }
 end
+
 
 # 1480. Running Sum of 1d Arrayは、与えられた1次元配列のランニングサムを計算する問題です。
 # ランニングサムとは、配列の各要素に対して、その要素までの全ての要素の合計を計算したものです。
@@ -53,15 +40,11 @@ end
 # 与えられた配列numsに対して、ランニングサムを計算し、新しい配列を返します。例えば、nums = [1, 2, 3, 4]の場合、ランニングサムは[1, 3, 6, 10]になります。
 
 # 解説
-# running_sum関数は、配列numsを引数として受け取り、ランニングサムを計算して返します。
-# (1...nums.length).each do |i|は、配列の2番目の要素から最後の要素までをループします。
-# nums[i] += nums[i - 1]は、現在の要素に前の要素の値を加えます。これにより、現在の要素はその位置までのランニングサムになります。
-# 最終的に、ランニングサムが計算された配列numsを返します。
 
-# 例
-# この例では、nums = [1, 2, 3, 4]に対してランニングサムを計算すると、[1, 3, 6, 10]が得られます。各ステップでの計算は以下の通りです：
+# 1. sum = 0:
+# - sum変数を初期化します。この変数はランニングサムを保持します。
 
-# nums[1] += nums[0] -> 2 + 1 = 3 -> nums = [1, 3, 3, 4]
-# nums[2] += nums[1] -> 3 + 3 = 6 -> nums = [1, 3, 6, 4]
-# nums[3] += nums[2] -> 4 + 6 = 10 -> nums = [1, 3, 6, 10]
-# このようにして、配列のランニングサムを計算します。
+# 2. nums.map { |num| sum += num }:
+# - mapメソッドを使って、各要素に対してランニングサムを計算します。
+# - 各要素numに対して、sumにその値を加え、sumの新しい値を返します。
+# - これにより、元の配列の各要素に対してランニングサムが計算された新しい配列が得られます。
