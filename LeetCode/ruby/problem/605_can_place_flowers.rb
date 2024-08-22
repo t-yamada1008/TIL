9d50e1a5e07a085959fe773435d1d7a97ab108b2
TakatoshiYamada <takatoshiyamada.3505@gmail.com>
2024-08-23 00:40:48 +0900

    Add: 問題分追加

--- LeetCode/ruby/problem/605_can_place_flowers.rb
+++ LeetCode/ruby/problem/605_can_place_flowers.rb
@@ -1,34 +1,29 @@
 # 605._Can_Place_Flowers
+# - Easy
 
-# 強引にクリアしたけど、もっとスマートな方法があるはず
+# You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+# Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+# 長い花壇があり、その一部には花が植えられており、一部には植えられていません。しかし、隣接する区画には花を植えることができません。
+# 整数配列 flowerbed が与えられ、この配列は 0 と 1 で構成されています。0 は空の区画を、1 は花が植えられている区画を意味します。また、整数 n が与えられます。この n 本の新しい花を、隣接する区画に花を植えないというルールを守りながら花壇に植えることができる場合は true を、できない場合は false を返してください。
+
+# ## Example 1:
+# - Input: flowerbed = [1,0,0,0,1], n = 1
+# - Output: true
+
+# ## Example 2:
+# - Input: flowerbed = [1,0,0,0,1], n = 2
+# - Output: false
+
+# ## Constraints:
+# - 1 <= flowerbed.length <= 2 * 104
+# - flowerbed[i] is 0 or 1.
+# - There are no two adjacent flowers in flowerbed.
+# - 0 <= n <= flowerbed.length
+
+## Code
+
 # @param {Integer[]} flowerbed
 # @param {Integer} n
 # @return {Boolean}
 def can_place_flowers(flowerbed, n)
-  result = 0
-  flowerbed.each_with_index do |f, i|
-      if i == 0
-          if flowerbed[i] == 0 && flowerbed[i + 1] == 0
-              result = result + 1
-              flowerbed[i] = 1
-          elsif flowerbed.length == 1
-              if f == 0
-                  result = result + 1
-                  flowerbed[i] = 1
-              end
-          end
-      elsif i == flowerbed.length - 1
-          if flowerbed[i] == 0 && flowerbed[i - 1] == 0
-              result = result + 1
-              flowerbed[i] = 1
-          end
-      else
-          if flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] ==0
-              result = result + 1
-              flowerbed[i] = 1
-          end
-      end
-  end
-
-  n <= result ? true : false
 end
